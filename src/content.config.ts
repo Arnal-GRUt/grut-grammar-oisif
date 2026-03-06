c48687d8328b845bcfe0408a1c7eabdd39866edd
GRUt. <reverendgrut@hotmail.com>
2026-03-06 11:34:45 +0100

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,29 +1,18 @@
-// import { defineCollection } from 'astro:content';
-// import { glob, file } from 'astro/loaders'; // Non disponible avec l'API héritée
-// import { z } from 'astro/zod';
+import { defineCollection } from 'astro:content';
+import { file } from 'astro/loaders'; // Non disponible avec l'API héritée
+import { z } from 'astro/zod';
 
-// const blog = defineCollection({
-//     loader: glob({ pattern: "**/*.md", base: "./src/data/blog" }),
-//     schema: /* ... */
-// });
-// const dogs = defineCollection({
-//     loader: file("src/data/dogs.json"),
-//     schema: /* ... */
-// });
+const artworks = defineCollection({
+    // `loader` peut accepter un tableau de plusieurs modèles ainsi que des modèles de chaîne de caractères
+    // Chargez tous les fichiers Markdown dans le répertoire space-probes, à l'exception de ceux qui commencent par « voyager- »
+    loader: file({ pattern: ['*.{jpg,jpeg,JPG,png,webp}'], base: 'src/artwork' }),
+    schema: z.object({        
+        name: z.string(),                
+        artist: z.string(),
+        description: z.string(),
+        inte_date: z.date(),
+        art_classe: z.enum(['numerique', 'trad']),        
+    }),
+});
 
-// const probes = defineCollection({
-//     // `loader` peut accepter un tableau de plusieurs modèles ainsi que des modèles de chaîne de caractères
-//     // Chargez tous les fichiers Markdown dans le répertoire space-probes, à l'exception de ceux qui commencent par « voyager- »
-//     loader: glob({ pattern: ['*.md', '!voyager-*'], base: 'src/data/space-probes' }),
-//     schema: z.object({
-//         name: z.string(),
-//         type: z.enum(['Space Probe', 'Mars Rover', 'Comet Lander']),
-//         launch_date: z.date(),
-//         status: z.enum(['Actif', 'Inactif', 'Déclassé']),
-//         destination: z.string(),
-//         operator: z.string(),
-//         notable_discoveries: z.array(z.string()),
-//     }),
-// });
-
-// export const collections = { blog, dogs, probes };
+export const artCollection = { artworks };

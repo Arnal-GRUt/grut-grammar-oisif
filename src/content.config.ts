623e0ff224b2c2aedcb37249d82891cb51c7a585
GRUt. <reverendgrut@hotmail.com>
2026-03-06 15:26:40 +0100

    test json images

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,24 @@
 import { defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
-// import { file } from 'astro/loaders'; // Non disponible avec l'API héritée
+// import { image } from 'astro:content';
+import { file } from 'astro/loaders'; // Non disponible avec l'API héritée
 import { z } from 'astro/zod';
+// style: z.enum(['Numérique', 'Trad', 'Mix']),
 
 const artworks = defineCollection({
     // `loader` peut accepter un tableau de plusieurs modèles ainsi que des modèles de chaîne de caractères
     // Chargez tous les fichiers Markdown dans le répertoire space-probes, à l'exception de ceux qui commencent par « voyager- »
-    loader: glob({ pattern: ['*.{jpg,jpeg,JPG,png,webp}'], base: 'src/artwork' }),
-    schema: z.object({        
-        name: z.string(),                
-        artist: z.string(),
-        description: z.string(),
-        inte_date: z.date(),
-        art_classe: z.enum(['numerique', 'trad']),        
+    // loader: glob({ pattern: ['*.{jpg,jpeg,JPG,png,webp}'], base: 'src/artwork' }),
+    loader: file("src/data/grutArt.json"),
+    schema ({image}) => z.object({        
+        dessin: z.object({
+            img_path: z.string(),
+            img_alt: z.string(),
+            format: z.string()
+        }),
+        date: z.date(),
+        auteur: z.string(),
+        style: z.string(),       
     }),
 });
-
+  
 export const artCollection = { artworks };

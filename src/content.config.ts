c746c81650232222dfd6d3b253755d4aaf44eb1b
GRUt. <reverendgrut@hotmail.com>
2026-03-06 11:38:23 +0100

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,12 @@
 import { defineCollection } from 'astro:content';
-import { file } from 'astro/loaders'; // Non disponible avec l'API héritée
+import { glob } from 'astro/loaders';
+// import { file } from 'astro/loaders'; // Non disponible avec l'API héritée
 import { z } from 'astro/zod';
 
 const artworks = defineCollection({
     // `loader` peut accepter un tableau de plusieurs modèles ainsi que des modèles de chaîne de caractères
     // Chargez tous les fichiers Markdown dans le répertoire space-probes, à l'exception de ceux qui commencent par « voyager- »
-    loader: file({ pattern: ['*.{jpg,jpeg,JPG,png,webp}'], base: 'src/artwork' }),
+    loader: glob({ pattern: ['*.{jpg,jpeg,JPG,png,webp}'], base: 'src/artwork' }),
     schema: z.object({        
         name: z.string(),                
         artist: z.string(),

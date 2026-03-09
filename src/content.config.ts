a128ccc4759134e277f1e8df81ac96a261aa2dd6
GRUt. <196240851+Arnal-GRUt@users.noreply.github.com>
2026-03-10 00:17:14 +0100

    code qui marche gribouille

---
+++ src/content.config.ts
@@ -0,0 +1,23 @@
+import { defineCollection } from 'astro:content';
+import { file } from 'astro/loaders';
+import { z } from 'astro/zod';
+// import { number } from 'astro:schema';
+
+const artwork = defineCollection({
+    loader: file("src/data/grutArt.json"),
+    schema: ({ image }) => z.object({
+        img_image: z.object({
+            img_path: z.string(),
+            img_alt: z.string()
+        }),
+        img_date: z.string()
+    }),
+});
+
+export const collections = { grutArt: artwork };
+// date: z
+//     .date()
+//     .transform((str) => new Date(str))
+//     .optional(),
+// style: z.enum(['Numérique', 'Trad', 'Mix']),
+// import { image } from 'astro:content';

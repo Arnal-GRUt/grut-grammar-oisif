c75408c2b2b87868f52832d4824dce92e747e762
GRUt. <reverendgrut@hotmail.com>
2026-03-10 15:23:12 +0100

    test nv schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,16 +3,25 @@ import { file } from 'astro/loaders';
 import { z } from 'astro/zod';
 // import { number } from 'astro:schema';
 
+// const artwork = defineCollection({
+//     loader: file("src/artworks/grutArt.json"),
+//     schema: ({ image }) => z.object({
+//         img_image: z.object({
+//             img_path: z.string(),
+//             img_alt: z.string()
+//         }),
+//         img_date: z.string()
+//     }),
+// });
 const artwork = defineCollection({
     loader: file("src/artworks/grutArt.json"),
     schema: ({ image }) => z.object({
-        img_image: z.object({
-            img_path: z.string(),
-            img_alt: z.string()
-        }),
+        img_path: z.string(),
+        img_alt: z.string(),
         img_date: z.string()
     }),
 });
+
 
 export const collections = { grutArt: artwork };
 // date: z

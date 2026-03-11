71ada2b93b058f71f94a8a64f700b98972e87532
GRUt. <reverendgrut@hotmail.com>
2026-03-11 14:42:30 +0100

    json config schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,9 +7,10 @@ const artwork = defineCollection({
     loader: file("src/images/grutArt.json"),
     schema: ({ image }) => z.object({
         image: z.object({
-            source: image(),
+            source: image(),            
             img_alt: z.string()
         }),
+        img_name: z.string(),
         img_date: z.string()
     }),
 });

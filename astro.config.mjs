abd7a67e779fb1f1ce143e5e72eeb3413b2ac17e
GRUt. <reverendgrut@hotmail.com>
2026-03-18 14:43:05 +0100

    add font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import markdoc from '@astrojs/markdoc';
-
-
 
 // https://astro.build/config
 export default defineConfig({
         integrations: [markdoc({ allowHTML: true })],
+        fonts: [{
+        provider: fontProviders.local(),
+        name: "ChelseaMarket-Regular",
+        cssVariable: "--font-chelsea-market",
+        options: {
+        variants: [{
+                src: ['./src/fonts/ChelseaMarket-Regular.ttf'],
+                weight: 'normal',
+                style: 'normal'
+        }]
+        }
+  }]
 });

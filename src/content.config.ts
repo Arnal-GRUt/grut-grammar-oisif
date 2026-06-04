c10ccf7a32025a91459193c34bec0d30bf464e97
GRUt. <196240851+Arnal-GRUt@users.noreply.github.com>
2026-06-04 23:29:38 +0200

    maj webouillle

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,7 +7,7 @@ const artwork = defineCollection({
     loader: file("src/images/grutArt.json"),
     schema: ({ image }) => z.object({
         image: z.object({
-            source: image(),            
+            source: image(),
             img_alt: z.string()
         }),
         img_name: z.string(),
@@ -15,7 +15,19 @@ const artwork = defineCollection({
     }),
 });
 
-export const collections = { grutArt: artwork };
+const webImg = defineCollection({
+    loader: file("src/images/visuelsWebouille.json"),
+    schema: ({ image }) => z.object({
+        image: z.object({
+            source: image(),
+            img_alt: z.string()
+        }),
+        img_name: z.string(),
+    }),
+});
+
+export const collections = { grutArt: artwork, visuelsWebouille: webImg };
+// export const collections = { grutArt: artwork };
 // date: z
 //     .date()
 //     .transform((str) => new Date(str))

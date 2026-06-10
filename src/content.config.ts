dc14bddaa22bda6c3e833a2437c27cb9bb12d185
GRUt. <reverendgrut@hotmail.com>
2026-06-10 16:10:25 +0200

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -26,5 +26,5 @@ const webImg = defineCollection({
     }),
 });
 
-export const collections = { grutArt : artwork, webImg };
+export const collections = { grutArt : artwork, visuelsWebouille : webImg };
 // export const collections = { grutArt: artwork };

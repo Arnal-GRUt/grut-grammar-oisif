4d4409e0b3c4d7de27fd8c1ff82d208b6c475c7b
GRUt. <196240851+Arnal-GRUt@users.noreply.github.com>
2026-08-04 20:05:21 +0200

    rss maj 2

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,8 +4,8 @@ export async function GET(context) {
   return rss({
     title: 'Les mots du Grammar Oisif',
     description: 'Des textes, des histoires, de la méthodo et cie',
-    // site: 'https://grammar-oisif.fr/',
-    site: 'http://localhost:4321/',
+    site: 'https://grammar-oisif.fr/',
+    // site: 'http://localhost:4321/',
     items: await pagesGlobToRssItems(
       import.meta.glob('./posts/*.{md,mdx}'),
       ),

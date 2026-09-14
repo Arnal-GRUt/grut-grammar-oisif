6183bb0dbb559410255a59b202637c544fbc73ee
GRUt. <reverendgrut@hotmail.com>
2026-09-14 09:47:26 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
-import markdoc from '@astrojs/markdoc';
+// import markdoc from '@astrojs/markdoc';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-        integrations: [markdoc({ allowHTML: true }), mdx()],
+        // integrations: [markdoc({ allowHTML: true }), mdx()],
+        integrations: [mdx()],
         fonts: [{
         provider: fontProviders.local(),
         name: "ChelseaMarket-Regular",

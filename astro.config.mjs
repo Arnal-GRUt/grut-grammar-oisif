1c670dd4c9eb74404cd353964d3557a757bc6bb4
GRUt. <reverendgrut@hotmail.com>
2026-09-14 16:26:37 +0200

    test modif css

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,16 +6,4 @@ import markdoc from '@astrojs/markdoc';
 // https://astro.build/config
 export default defineConfig({
         integrations: [markdoc({ allowHTML: true })]
-        // fonts: [{
-        //         provider: fontProviders.local(),
-        //         name: "ChelseaMarket-Regular",
-        //         cssVariable: "--font-chelsea-market",
-        //         options: {
-        //                 variants: [{
-        //                         src: ['./src/fonts/ChelseaMarket-Regular.ttf'],
-        //                         weight: 'normal',
-        //                         style: 'normal'
-        //                 }]
-        //         }
-        // }]
 });

7dfbe6779f8254a7667194ff33fdb2ce89a35123
GRUt. <reverendgrut@hotmail.com>
2026-09-14 10:54:29 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,17 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
         // integrations: [markdoc({ allowHTML: true }), mdx()],
-        integrations: [mdx()],
-        fonts: [{
-        provider: fontProviders.local(),
-        name: "ChelseaMarket-Regular",
-        cssVariable: "--font-chelsea-market",
-        options: {
-        variants: [{
-                src: ['./src/fonts/ChelseaMarket-Regular.ttf'],
-                weight: 'normal',
-                style: 'normal'
-        }]
-        }
-  }]
+        integrations: [mdx()]
+        // fonts: [{
+        //         provider: fontProviders.local(),
+        //         name: "ChelseaMarket-Regular",
+        //         cssVariable: "--font-chelsea-market",
+        //         options: {
+        //                 variants: [{
+        //                         src: ['./src/fonts/ChelseaMarket-Regular.ttf'],
+        //                         weight: 'normal',
+        //                         style: 'normal'
+        //                 }]
+        //         }
+        // }]
 });
